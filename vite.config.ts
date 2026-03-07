af928e4aab8a24206af827c6a55fb39cf8c028d4
Robin <robin.p.winton@gmail.com>
2026-03-07 13:51:42 -0500

    Add TypeScript config and simplify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: './',
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html')
-    }
   },
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
